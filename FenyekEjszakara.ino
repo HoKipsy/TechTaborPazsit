6234fef8d4738ef22da7ae9f55d2deaa7a3fd137
HoKipsy <remenyireka2000@gmail.com>
2017-10-06 20:16:20 +0200

    Add files via upload

---
+++ FenyekEjszakara.ino
@@ -0,0 +1,17 @@
+void setup(void) {
+  pinMode(A0, INPUT);   //fenyszenzor bemenete
+  pinMode(13, OUTPUT);  //LEDek kimenete
+  Serial.begin(9600);
+}
+
+void loop(void) {
+  int fenyero = analogRead(A0);
+  Serial.print("Fenyero = ");
+  Serial.println(fenyero);
+
+  if(fenyero < 500) {
+      digitalWrite(13, HIGH);
+    } else {
+        digitalWrite(13, LOW);
+      }
+  delay(200);
